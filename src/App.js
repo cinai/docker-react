39ff88305913c9de1001b694fd3337dca8bd51e9
cinai <ceinaipil@gmail.com>
2019-06-18 16:59:45 -0400

    cambiar el texto

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>De git to travis to aws</h2>
         </div>
         <p className="App-intro">
           Hola ale jeje
